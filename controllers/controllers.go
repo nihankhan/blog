25518b910501b67ca1e1de28856fb68dc4f79e5f
nihankhan <nihan.khan@outlook.com>
2023-02-01 20:33:48 +0600

    create some function

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,9 +5,12 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/gosimple/slug"
 	"github.com/nihankhan/blog/helpers"
 	"github.com/nihankhan/blog/models"
 	"github.com/nihankhan/blog/views"
@@ -143,5 +146,153 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
+	permissson(w, r)
 
+	switch r.Method {
+	case http.MethodGet:
+		var resp bytes.Buffer
+
+		if err := views.Tmpl.ExecuteTemplate(&resp, "login.tpl", nil); err != nil {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+
+		io.WriteString(w, resp.String())
+
+	case http.MethodPost:
+		user := &models.User{
+			Email: r.FormValue("email"),
+		}
+
+		user = user.Find()
+
+		if user.ID == 0 {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+
+			return
+		}
+
+		err := bcrypt.CompareHashAndPassword(user.Password, []byte(r.FormValue("password")))
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+
+			return
+		}
+
+		sessionsID := uuid.New().String()
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: sessionsID,
+		}
+		Sessions[sessionsID] = user
+
+		http.SetCookie(w, cookie)
+		http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+	}
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	cookie := getCookie(r)
+
+	if Sessions[cookie.Value] != nil {
+		delete(Sessions, cookie.Value)
+	}
+
+	cookie = &http.Cookie{
+		Name:  "session",
+		Value: "",
+	}
+	http.SetCookie(w, cookie)
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
+
+func Dashboard(w http.ResponseWriter, r *http.Request) {
+	permissson(w, r)
+
+	cookie := getCookie(r)
+	user := Sessions[cookie.Value]
+	model := r.URL.Query().Get("model")
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+	delete := r.URL.Query().Get("delete")
+
+	switch r.Method {
+	case http.MethodGet:
+		switch model {
+		case "article":
+			if delete != "" && id != 0 {
+				article := &models.Article{
+					ID: id,
+				}
+
+				user.DeleteArticle(article)
+
+				http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+			} else {
+				data.Article = &models.Article{
+					Image:   "",
+					Title:   "",
+					Content: "",
+				}
+
+				if user != nil && id != 0 {
+					data.Article = user.FindArticle(id)
+				}
+
+				var resp bytes.Buffer
+
+				if err := views.Tmpl.ExecuteTemplate(&resp, "article.tpl", data); err != nil {
+					http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+
+					return
+				}
+
+				io.WriteString(w, resp.String())
+			}
+
+		default:
+			if user != nil {
+				data.Articles = user.FindArticles()
+			}
+
+			var resp bytes.Buffer
+
+			if err := views.Tmpl.ExecuteTemplate(&resp, "dashboard.tpl", data); err != nil {
+				http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+
+				return
+			}
+
+			io.WriteString(w, resp.String())
+		}
+	case http.MethodPost:
+		switch model {
+		case "article":
+			if user != nil {
+				if id == 0 {
+					article := &models.Article{
+						Image: r.FormValue("image"),
+						Slug:  slug.Make(r.FormValue("title")),
+						Title: r.FormValue("title"),
+						Content: r.FormValue("content"),
+						Author: *user,
+						CreatedAt: time.Now(),
+					}
+
+					user.CreateArticle(article)
+				} else {
+					article := &models.Article{
+						ID: id,
+						Image: r.FormValue("image"),
+						Slug: slug.Make(r.FormValue("title")),
+						Title: r.FormValue("title"),
+						Content: r.FormValue("content"),
+					}
+
+					user.UpdateArticle(article)
+				}
+			}
+
+			http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.18
 
 require (
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/gosimple/slug v1.13.1 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
 )
